1b34ac0a473b33479dd15c6c55bb7a80c03ee43b
Faith <faithruetas@gmail.com>
2024-07-12 17:41:14 -0400

    prelim bullet -> flowers transition/fade feat

--- script.js
+++ script.js
@@ -7,7 +7,28 @@ function toggleMenu() {
   
     // Element.classList: returns a live collection of the class attributes
     //      of the elements
-    // toggle(): if "open" is set remove it, otherwise add it
+    // toggle(): if "open" is set remove it, otherwise add it in style.css
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+// on hover, bullets become pixel flowers
+const exp_list_items = document.querySelectorAll('.experience-list-item')
+
+// bullets.forEach((cur_bullet) => cur_bullet.addEventListener('mouseover', e)
+
+exp_list_items.forEach(function(cur_item) {
+
+    let cur_bullet_div = cur_item.querySelector('.fa-bullet');
+    let cur_flower = cur_item.querySelector('.pixel-symbol-bullet');
+
+    cur_item.addEventListener('mouseover', (e) => {
+        cur_bullet_div.classList.toggle('hide-item');
+        cur_flower.classList.toggle('hide-item');
+    });
+
+    cur_item.addEventListener('mouseout', (e) => {
+        cur_bullet_div.classList.toggle('hide-item');
+        cur_flower.classList.toggle('hide-item');
+    });
+});
